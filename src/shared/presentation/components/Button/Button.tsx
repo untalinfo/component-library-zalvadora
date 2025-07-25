f3f1fe372d56a3bb398f2e09bacbd52ccbbafb87
oscar Info <racso1607@gmail.com>
2025-07-25 06:53:08 -0500

    refactor button css module styles

--- src/shared/presentation/components/Button/Button.tsx
+++ src/shared/presentation/components/Button/Button.tsx
@@ -1,32 +1,32 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import './Button.scss';
+import React from "react";
+import Button from "@mui/material/Button";
+import styles from "./Button.module.scss";
 
 export interface CustomButtonProps {
   children: React.ReactNode;
   className?: string;
-  variant?: 'text' | 'outlined' | 'contained';
-  color?: 'primary' | 'secondary' | 'error' | 'info' | 'success' | 'warning';
-  size?: 'small' | 'medium' | 'large';
+  variant?: "text" | "outlined" | "contained";
+  color?: "primary" | "secondary" | "error" | "info" | "success" | "warning";
+  size?: "small" | "medium" | "large";
   disabled?: boolean;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
-  type?: 'button' | 'submit' | 'reset';
+  type?: "button" | "submit" | "reset";
 }
 
 const CustomButton: React.FC<CustomButtonProps> = ({
   children,
   className,
-  variant = 'contained',
-  color = 'primary',
-  size = 'medium',
+  variant = "contained",
+  color = "primary",
+  size = "medium",
   disabled = false,
   onClick,
-  type = 'button',
+  type = "button",
   ...rest
 }) => {
   return (
     <Button
-      className={`custom-button-lib ${className || ''}`}
+      className={`${styles.customButtonLib} ${className || ""}`}
       variant={variant}
       color={color}
       size={size}
